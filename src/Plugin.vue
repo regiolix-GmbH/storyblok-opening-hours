b207476e850f65267b0c02571a6b40eaf179de6d
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-12-17 13:31:42 +0100

    Initial commit

---
+++ src/Plugin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="OpeningHours"/>
+</template>
+
+<script>
+export default {
+  mixins: [window.Storyblok.plugin],
+  watch: {
+    model: {
+      deep: true,
+      handler(value) {
+        // Let Storyblok know that the value was updated.
+        this.$emit(`changed-model`, value);
+      },
+    },
+  },
+  methods: {
+    initWith() {
+      return {
+        // This array will be filled with the opening
+        // hours for every specific day of the week.
+        days: [],
+        // This is the name of our plugin.
+        plugin: `opening-hours`,
+      };
+    },
+  },
+};
+</script>
